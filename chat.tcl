aedca65bda362869b2f53c0fb5ca040a591cc43c
Ronsor <ronsor@openstar.pw>
2017-05-11 22:02:11 +0000

    first

---
+++ chat.tcl
@@ -0,0 +1,320 @@
+ # CONFIG HERE
+ 
+ set fname "chat.conf"
+ if { [lindex $::argv 0] ne "" } {
+	set fname [lindex $::argv 0]
+ }
+ puts "CONF $fname"
+ proc _LoadConf {} {
+ set o [open $::fname]
+ while { ![eof $o] } {
+ set x [gets $o]
+ if { [string index $x 0] ne "#" } {
+ array set ::conf [split $x "="]
+ }
+ }
+ close $o
+ set ::me $::conf(name)
+ set ::ports $::conf(ports)
+ set ::motd $::conf(motd)
+ array set ::mods $::conf(mods)
+}
+ # END CONFIG
+_LoadConf
+ if {![info exists sock]} {
+    set sock 1
+    foreach p $::ports {
+    set sock$p [socket -server connect $p]
+    }
+ }
+ set usedtoks {}
+ proc gettok {} {
+   set _a #[clock clicks],[clock seconds]; lappend ::usedtoks $_a; return $_a
+ }
+proc uputs {sock text} {
+	if { ! [info exists ::issock($sock)] } {
+		sendToAllServer "$text"
+	} else {
+		puts $sock $text
+	}
+}
+ proc connect {sock host port {isserver 0}} {
+    fconfigure $sock -blocking 0 -buffering line
+    fileevent $sock readable [list handleSocket $sock]
+    set ::socks($sock) $sock
+    set ::chans($sock) {}
+    set ::issock($sock) 1
+    set ::hosts($sock) $host
+    if { $isserver eq 1 } {
+	puts $sock "[gettok] * SERVER"
+	puts $sock "[gettok] * BURST"
+	puts $sock "[gettok] * NICK $::me"
+    }
+    puts $sock "[gettok] $::me INFO :relaychat-1.0 Welcome to the Relay Chat Network!"
+    set f [open $::motd]
+    while { ![eof $f] } {
+	puts $sock "[gettok] $::me MOTD :[gets $f]"
+    }
+    close $f
+    puts $sock "[gettok] $::me INFO :You are now known as $sock"
+    sendToAllServer "[gettok] $::me FAKENICK fake$sock[clock clicks] $sock $host"
+}
+
+ proc disconnect {sock args} {
+    set nick $::socks($sock)
+    sendTextAllFor $sock "[gettok] $nick QUIT :[join $args]"
+    catch { puts $sock "[gettok] $::me ERROR :Closing Link: $::hosts($sock): [join $args]" }
+    unset ::socks($sock)
+    unset ::chans($sock)
+    unset ::hosts($sock)
+    catch { unset ::opers($sock) }
+    if { [info exists ::issock($sock) ] } {
+	catch { unset ::servers($sock) }
+    	unset ::issock($sock)
+	fileevent $sock readable {}
+	close $sock
+    } else {
+    sendToAllServer "[gettok] $::me KILL $nick :[join $args]"
+   }
+	
+    #sendText "* $nick has left the chat: [join $args]"
+ }
+ proc handleSocket {sock} {
+ catch {
+    gets $sock line
+    set rsock $sock
+    if {[eof $sock]} {
+	fileevent $sock readable {}
+	disconnect $sock {Connection reset by peer}
+    }
+    if {$line eq ""} return
+    set linexx [list {*}[split [lindex [split [string map {" :" ^} $line] "^"] 0] " "] [join [lrange [split [string map {" :" ^} $line] "^"] 1 end] " :"]]
+	if { [string index $line 0] == "#" } {
+		set thetok [lindex $linexx 0]
+		if { $thetok in $::usedtoks } return
+		lappend ::usedtoks $thetok
+		set src [lindex $linexx 1]
+		if { $src eq "*" } { set src $rsock }
+		if { [nick2id [lindex [split $src "@"] 0]] eq 0 } {
+		#puts $sock "[gettok] $::me WARNING :Your connection could be closed due to a protocol violation [!]"
+		if { [info exists ::servers($sock)] }  {
+		#disconnect $sock "Protocol violation: user does not exist (Bug?): $src"
+		} else {
+		return
+		}
+		}
+		set sock [nick2id $src]
+		set linex [lrange $linexx 2 end]
+	} else {
+		set thetok [gettok]
+		set src "$::socks($sock)@$::hosts($sock)"
+		set linex $linexx
+	}
+	    puts "$sock: $line"
+
+	switch -nocase [lindex $linex 0] {
+		SERVER {
+			puts $rsock "NICK $::me"
+			puts $rsock "BURST"
+			set ::servers($sock) $sock
+		}
+		BURST {
+			set ::servers($sock) $sock
+			puts "$::socks($sock) is now a server"
+			foreach {k v} [array get ::socks] {
+				puts $rsock "[gettok] $::me FAKENICK fake$k[clock clicks] $v $::hosts($k)"
+				foreach c $::chans($k) {
+				puts $rsock "[gettok] $v JOIN $c"
+				}
+			}
+			foreach c [array names ::cmods] {
+				foreach l $::cmods($c) {
+				puts $rsock "[gettok] $::me MOD $c $::socks($l)"
+				}
+				puts $rsock "[gettok] $::me TOPIC $c :$::topics($c)"
+			}
+		}
+		LIST {
+			foreach {c} [array names ::topics] {
+				puts $rsock "[gettok] $::me LIST $c"
+			}
+		}
+		FAKENICK {
+			set ::socks([lindex $linex 1]) [lindex $linex 2]
+			set ::chans([lindex $linex 1]) {}
+			set ::hosts([lindex $linex 1]) [lindex $linex 3]
+			sendToAllServer "$thetok $src FAKENICK [lindex $linex 1] [lindex $linex 2] [lindex $linex 3]"
+		}
+		GLOBAL {
+			puts "GLOBALING"
+			sendText "$thetok $src GLOBAL :[lindex $linex 1]"
+		}
+		MODLOGIN {
+			if { [info exists ::mods([lindex $linex 1])] && $::mods([lindex $linex 1]) ne [lindex $linex 2] } {
+			puts $rsock "$thetok $::me ERROR :You've failed to authenticate as a Global Moderator!"
+			} else {
+			set ::opers($sock) "Global Moderator"
+			sendText "$thetok $src GLOBAL :$::socks($sock)@$::hosts($sock) is now a global moderator"
+			}
+		}
+		KILL {
+			if { [nick2id [lindex $linex 1]] ne 0 && ( [info exists ::opers($sock)] || [info exists ::servers($rsock)] ) } {
+			disconnect [nick2id [lindex $linex 1]] [lindex $linex 2]
+			}
+		}
+		JOIN {
+			if { ! [info exists ::topics([string tolower [lindex $linex 1]]) ] } {
+				set ::topics([string tolower [lindex $linex 1]]) "No Topic Set"
+				set ::cmods([string tolower [lindex $linex 1]]) $sock
+			}
+			lappend ::chans($sock) [string tolower [lindex $linex 1]]
+			sendTextToChan [string tolower [lindex $linex 1]] "$thetok $src JOIN [string tolower [lindex $linex 1]]"
+			puts $rsock "[gettok] $::me TOPIC [string tolower [lindex $linex 1]] :$::topics([string tolower [lindex $linex 1]])"
+		}
+		TOPIC {
+			if { [lindex $linex 2] eq "" } {
+			puts $rsock "[gettok] $::me TOPIC [string tolower [lindex $linex 1]] :$::topics([string tolower [lindex $linex 1]])"
+			}
+			if { ($sock in $::cmods([string tolower [lindex $linex 1]]) || [info exists ::servers($rsock)]) && [lindex $linex 2] ne "" } {
+			set ::topics([string tolower [lindex $linex 1]]) [lindex $linex 2]
+			sendTextToChan "$thetok $src TOPIC [string tolower [lindex $linex 1]] :[lindex $linex 2]"
+			}
+		}
+		PART {
+			sendTextToChan [string tolower [lindex $linex 1]] "$thetok $src PART [string tolower [lindex $linex 1]]"
+			set ::chans($sock) [lsearch -all -not -exact -inline $::chans($sock) [string tolower [lindex $linex 1]]]
+		}
+		KICK {
+			if { [nick2id [lindex $linex 2]] eq 0 } return
+			if { $sock in $::cmods([string tolower [lindex $linex 1]]) || [info exists ::servers($rsock)]} {
+		sendTextToChan [string tolower [lindex $linex 1]] "$thetok $src KICK [string tolower [lindex $linex 1]] [lindex $linex 2]"
+			set ::chans([nick2id [lindex $linex 2]]) [lsearch -all -not -exact -inline $::chans([nick2id [lindex $linex 2]]) [string tolower [lindex $linex 1]]]
+			} else {
+				puts $rsock "$thetok $::me ERROR :You don't have permission to do that"
+			}
+
+		}
+		MOD {
+			if { [nick2id [lindex $linex 2]] eq 0 } return
+			if { $sock in $::cmods([string tolower [lindex $linex 1]]) || [info exists ::servers($rsock)]} {
+				sendTextToChan [string tolower [lindex $linex 1]] "$thetok $src MOD [string tolower [lindex $linex 1]] [lindex $linex 2]"
+		lappend ::cmods([string tolower [lindex $linex 1]]) [nick2id [lindex $linex 2]]
+			} else {
+				puts $rsock "$thetok $::me ERROR :You don't have permission to do that"
+			}
+
+		}
+		DEMOD {
+			if { [nick2id [lindex $linex 2]] eq 0 } return
+			if { $sock in $::cmods([string tolower [lindex $linex 1]]) || [info exists ::servers($rsock)]} {
+				sendTextToChan [string tolower [lindex $linex 1]] "$thetok $src DEMOD [string tolower [lindex $linex 1]] [lindex $linex 2]"
+		set ::cmods([string tolower [lindex $linex 1]]) [lsearch -inline -all -not -exact $::cmods([string tolower [lindex $linex 1]]) [nick2id [lindex $linex 2]]]
+			} else {
+				puts $rsock "$thetok $::me ERROR :You don't have permission to do that"
+			}
+		}
+		NICK {
+			if { [lsearch -nocase [array get ::socks] [lindex $linex 1]] != -1 } {
+				puts $rsock "$thetok $::me ERROR :Nickname in use"
+			} else {
+				sendTextAllFor $sock "$thetok $src NICK [lindex $linex 1]"
+				set ::socks($sock) [lindex $linex 1]
+			}
+		}
+		CONNECT {
+			if { [info exists ::opers($sock)] } {
+			set host [lindex $linex 1]
+			set port [lindex $linex 2]
+	if { ! [catch { set sock [socket $host $port] }] } {
+	connect $sock $host $port 1
+	}
+			sendText "$thetok $src GLOBAL :is connecting to remote server $host:$port"
+
+			}
+		}
+		QUIT {
+			disconnect $sock [lindex $linex 1]
+		}
+		WHOIS {
+			if { [nick2id [lindex $linex 1]] eq 0 } return
+			puts $rsock "$thetok $::me ABOUT [lindex $linex 1] [nick2id [lindex $linex 1]] $::hosts([nick2id [lindex $linex 1]]) :is my nickname, local uid, and hostname"
+		}
+		USERS {
+	set chan [lindex $linex 1]
+	set users {}
+    foreach s [array names ::chans] {
+        if { [string tolower $chan] in $::chans($s) } {
+	set c ""
+	if { $s in $::cmods([string tolower $chan]) } { set c "*" }
+           lappend users $c$::socks($s)
+        }
+    }
+		puts $rsock "$thetok $::me USERS [lindex $linex 1] :[join $users]"
+		}
+		MESSAGE {
+			if { [nick2id [lindex $linex 1]] eq 0 } {
+			sendTextToChan [string tolower [lindex $linex 1]] "$thetok $src MESSAGE [string tolower [lindex $linex 1]] :[lindex $linex 2]" $sock
+			} else {
+			uputs [nick2id [lindex $linex 1]] "$thetok $src MESSAGE [string tolower [lindex $linex 1]] :[lindex $linex 2]"
+			}
+
+		}
+	}
+#        sendText "$::socks($sock): $line"
+}
+ }
+ proc sendTextAllFor {sock text} {
+   if { $sock != 0 } {
+   catch { puts $sock $text }
+   set done {}
+   foreach c $::chans($sock) {
+   #   sendTextToChan $c $text $sock
+      foreach s [array names ::chans] {
+	 if { [string tolower $c] in $::chans($s) && [info exists ::issock($s)] && $s ne $sock && $s ni $done } {
+		lappend done $s
+		puts $s $text
+         }
+      }
+  }
+  }
+  sendToAllServer $text
+ }
+ proc nick2id {n} {
+	foreach s [array names ::socks] { 
+		if { [string tolower $::socks($s)] eq [string tolower $n] } {
+			return $s
+		}
+	}
+	return 0
+ }
+ proc sendToAllServer {text} {
+	foreach s [array names ::servers] {
+		puts "$s: $text"
+		puts $s $text
+	}
+ }
+ proc sendText {text} {
+    foreach s [array names ::socks] {
+	if { [info exists ::issock($s) ] } {
+        puts $s $text
+	}
+    }
+ }
+ proc sendTextToChan {chan text {except ""}} {
+    foreach s [array names ::chans] {
+        if { [string tolower $chan] in $::chans($s) && [info exists ::issock($s)] && $s ne $except } {
+           puts $s $text
+        }
+    }
+    foreach s [array names ::servers] {
+	puts $s $text
+    }
+ }
+
+foreach {host port} $::conf(connects) {
+	if { ! [catch { set sock [socket $host $port] }] } {
+	connect $sock $host $port 1
+	}
+
+}
+ vwait forever
---
+++ chatclient.tcl
@@ -0,0 +1,152 @@
+# simple chat client
+array set ::wins {status ""}
+proc out {win text} {
+	if { ! [info exists ::wins($win)] } { set ::wins($win) {} }
+	lappend ::wins($win) $text
+	if { $::curwin eq $win } { puts $text }
+}
+proc outall {text} {
+	foreach c [array names ::wins] {
+		out $c $text
+	}
+}
+proc notify {tgtwin text} {
+	if { $::curwin ne $tgtwin } {
+		out $::curwin "-- Notification: $text --"
+	}
+}
+set ::curwin status
+proc handlein {cs} {
+set line [gets $cs]
+set linexx [list {*}[split [lindex [split [string map {" :" ^} $line] "^"] 0] " "] [join [lrange [split [string map {" :" ^} $line] "^"] 1 end] " :"]]
+switch -nocase -regexp -- [lindex $linexx 2] {
+	GLOBAL {
+	out status "< Global Message from [lindex $linexx 1] > [lindex $linexx 3]"
+	}
+	MESSAGE {
+	set pcx [lindex $linexx 1]
+	if { [string index $pcx 0] eq "#" } {
+		set pcx [linex $linexx 3]
+	}
+	out $pcx "([lindex $linexx 1]) [join [lrange $linexx 4 end]]"
+	notify $pcx "You have a message from [lindex $linexx 1]"
+	}
+	USERS {
+	out [lindex $linexx 3] "Users in [lindex $linexx 3]: [lindex $linexx 4]"
+	}
+	LIST {
+	out status "Channel: [lindex $linexx 3]"
+	}
+	(MOTD|INFO) {
+	out status "<server> [lindex $linexx 3]"
+	}
+	NICK {
+	outall "-- [lindex $linexx 1] is now known as [lindex $linexx 3] --"
+	}
+	JOIN {
+	out [lindex $linexx 3] "-- [lindex $linexx 1] has joined [lindex $linexx 3] --"
+	}
+	PART {
+	out [lindex $linexx 3] "-- [lindex $linexx 1] has left [lindex $linexx 3] --"
+	}
+	KICK {
+	out [lindex $linexx 3] "-- [lindex $linexx 4] was kicked from [lindex $linexx 3] by [lindex $linexx 1] --"
+	}
+	DEMOD {
+	out [lindex $linexx 3] "-- [lindex $linexx 4] is no longer a moderator in [lindex $linexx 3] (thanks to [lindex $linexx 1]) --"
+	}
+	MOD {
+	out [lindex $linexx 3] "-- [lindex $linexx 4] is now a moderator in [lindex $linexx 3] (thanks to [lindex $linexx 1]) --"
+	}
+	TOPIC {
+	out [lindex $linexx 3] "-- the topic for [lindex $linexx 3] is: [lindex $linexx 4] (set by [lindex $linexx 1]) --"
+	}
+	default {
+	out status "-- [join [lrange $linexx 1 end]] --"
+	}
+}
+}
+proc orc {a b} {
+	if { $a eq "" } {
+		return $b
+	}
+	return $a
+}
+proc handleout {cs} {
+	gets stdin dat
+	if { [string index $dat 0] == "/"} {
+	set cmd [lindex $dat 0]
+	switch -nocase [lindex $dat 0] {
+		/lwin {
+			set n 0
+			foreach c [array names ::wins] {
+			out $::curwin "Window ($n): $c"
+			incr n
+			}
+		}
+		/win {
+			if { [info exists ::wins([lindex [array names ::wins] [lindex $dat 1]])] } {
+			set ::curwin [lindex [array names ::wins] [lindex $dat 1]]
+			out $::curwin "-- Switched to $::curwin --"
+			foreach l $::wins($::curwin) {
+			puts $l
+			}
+			}
+		}
+		/nick {
+			puts $cs "NICK [lindex $dat 1]"
+		}
+		/join {
+			puts $cs "JOIN [lindex $dat 1]"
+			set ::curwin [string tolower [lindex $dat 1]]
+		}
+		/part {
+			puts $cs "PART [lindex $dat 1]"
+			unset ::wins([lindex $dat 1])
+			set ::curwin status
+		}
+		/list {
+		puts $cs "LIST"
+		}
+		/users {
+			puts $cs "USERS [orc [lindex $dat 1] $::curwin]"
+		}
+		/topic {
+			puts $cs "TOPIC [lindex $dat 1] :[lindex $dat 2]"
+		}
+		/demod {
+			puts $cs "DEMOD [lindex $dat 1] [lindex $dat 2]"
+		}
+		/mod {
+			puts $cs "MOD [lindex $dat 1] [lindex $dat 2]"
+		}
+		/kick {
+			puts $cs "TOPIC [lindex $dat 1] [lindex $dat 2]"
+		}
+		/whois {
+			puts $cs "WHOIS [lindex $dat 1]"
+		}
+		/nick {
+			puts $cs "NICK [lindex $dat 1]"
+		}
+		/msg {
+			puts $cs "MESSAGE [lindex $dat 1] :[lindex $dat 2]"
+		}
+		/help {
+			out status "Commands: /whois, /mod, /msg, /list, /kick, /demod, /join, /part, /lwin, /win"
+		}
+
+	}
+	} else {
+	if { $::curwin ne "status" } {
+		puts $cs "MESSAGE $::curwin :$dat"
+	}
+	}
+}
+set clientsock [socket [lindex $argv 0] [lindex $argv 1]]
+fconfigure $clientsock -buffering line -translation auto
+puts $clientsock "NICK [lindex $argv 2]"
+fileevent stdin readable [list handleout $clientsock]
+fileevent $clientsock readable [list handlein $clientsock ]
+
+vwait eternity
